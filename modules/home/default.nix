937578ee172746c44ecc3d3b45ecca903592b76f
adtr <adtr@users.noreply.github.com>
2026-06-05 17:54:52 +0200

    Set iTerm2 theme to dark by default

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   pkgs,
   user,
   ...
@@ -33,6 +34,27 @@
 
   # Let home-manager manage itself
   programs.home-manager.enable = true;
+
+  # Make iTerm2 open with a dark theme by default.
+  home.activation.iterm2DarkTheme = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    plist="$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+    if command -v python3 >/dev/null 2>&1 && [ -f "$plist" ]; then
+      python3 - "$plist" <<'PY'
+import pathlib
+import plistlib
+import sys
+
+path = pathlib.Path(sys.argv[1])
+with path.open('rb') as f:
+    data = plistlib.load(f)
+
+data['TabStyleWithAutomaticOption'] = 1
+
+with path.open('wb') as f:
+    plistlib.dump(data, f)
+PY
+    fi
+  '';
 
   # XDG base dirs
   xdg.enable = true;
